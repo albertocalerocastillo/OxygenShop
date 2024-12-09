443e2d3972510174af27d77f12471cf1199ce07a
albertocalerocastillo <albertocalerocastillo@gmail.com>
2024-12-09 13:50:15 +0100

    FIXED: nav, popup, pricing and footer design and functionality

--- scripts/return-top.js
+++ scripts/return-top.js
@@ -1,9 +1,26 @@
 const returnTop = document.getElementById('iconoUp');
 
+const toggleReturnTopButton = () => {
+    const scrollPosition = window.scrollY;
+    const scrollHeight = document.body.scrollHeight - window.innerHeight;
+    const scrollPercentage = (scrollPosition / scrollHeight) * 100;
+
+    if (scrollPercentage > 20) {
+        returnTop.style.display = 'block';
+    } else {
+        returnTop.style.display = 'none';
+    }
+};
+
+window.addEventListener('scroll', toggleReturnTopButton);
+
 const goTop = () => {
-    setTimeout(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, 200);
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 };
 
 returnTop.addEventListener('click', goTop);
+
+toggleReturnTopButton();
